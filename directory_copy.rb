abf18e9e6b13d358f1496f3e750fccec9cd8d175
Moeid Haider <moeidhaider9@gmail.com>
2023-01-09 08:48:08 +0000

    Added a filter to only print names starting with 's'

--- directory_copy.rb
+++ directory_copy.rb
@@ -9,7 +9,9 @@ end
 
 def print(students)
     students.each.with_index(1) do |student,index|
-        puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+        if student[:name][0] == "s"
+            puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+        end
     end
 end
 
