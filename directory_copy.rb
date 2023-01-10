f75e3312901aef504f0f96a44b387b4ac6f30809
Moeid Haider <moeidhaider9@gmail.com>
2023-01-10 12:12:37 +0000

    Used '.center' to edit layout of the code

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,41 +1,42 @@
+@width = 50 
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+    puts "The students of Villains Academy".center(@width)
+    puts "-------------".center(@width)
 end
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{students.count} great students".center(@width)
 end
 
 def print(students)
     students.each.with_index(1) do |student,index|
         if student[:name].length < 12
-            puts "#{student[:name]} (#{student[:cohort]} cohort)"
+            puts "#{student[:name]} (#{student[:cohort]} cohort)".center(@width)
         end
     end
     ## reworked each statement
     i = 0
     while i < students.count
-        puts "#{students[i][:name]} born in #{students[i][:country_of_birth]} (#{students[i][:cohort]})"
+        puts "#{students[i][:name]} born in #{students[i][:country_of_birth]} (#{students[i][:cohort]})".center(@width)
         i += 1
     end
 end
 
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish press return twice"
+    puts "Please enter the names of the students".center(@width)
+    puts "To finish press return twice".center(@width)
     students = []
     name = gets.chomp
-    puts "Please give the country of birth of the student"
+    puts "Please give the country of birth of the student".center(@width)
     country_of_birth = gets.chomp
     while !name.empty? do
         students << {name: name, cohort: :november, country_of_birth: country_of_birth}
-        puts "Now we have #{students.count} students"
+        puts "Now we have #{students.count} students".center(@width)
         name = gets.chomp
         if name.empty?
             break
         end
-        puts "Please give the country of birth of the student"
+        puts "Please give the country of birth of the student".center(@width)
         country_of_birth = gets.chomp
     end
     students
