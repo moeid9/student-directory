ce56acfa9fd703a0345e48dea878270ed508d28a
Moeid Haider <moeidhaider9@gmail.com>
2023-01-10 23:15:26 +0000

    Added a save to file function and a '.gitignore file'

--- directory.rb
+++ directory.rb
@@ -18,6 +18,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit"
 end
 
@@ -40,6 +41,8 @@ def process(selection)
             input_students
         when "2"
             show_students
+        when "3"
+            save_students
         when "9"
             exit
         else
@@ -56,6 +59,15 @@ def input_students
         puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
+end
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
 end
 
 interactive_menu
