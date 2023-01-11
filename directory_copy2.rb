7414659d8c5cde397b37592eccf7420cd24732e2
Moeid Haider <moeidhaider9@gmail.com>
2023-01-11 23:11:08 +0000

    further refactored code

--- directory_copy2.rb
+++ directory_copy2.rb
@@ -1,32 +1,43 @@
 @students = []
+@width = 75
+
+def divider
+    puts "-------------".center(@width)
+end
 
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+    if !@students.empty?
+        puts "The students of Villains Academy".center(@width)
+        divider
+    end
 end
 
 def print_footer
-    puts "Overall, we have #{@students.count} great students"
+    puts "Overall, we have #{@students.count} great students".center(@width)
 end
 
 def print_students_list
     @students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+        puts "#{student[:name]} (#{student[:cohort]} cohort)".center(@width)
     end
 end
 
 def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Save the list to students.csv"
-    puts "4. Load the list from students.csv"
-    puts "9. Exit"
+    puts "1. Input the students".center(@width)
+    puts "2. Show the students".center(@width)
+    puts "3. Save the list to students.csv".center(@width)
+    puts "4. Load the list from students.csv".center(@width)
+    puts "9. Exit".center(@width)
 end
 
 def show_students
-    print_header
-    print_students_list
-    print_footer
+    if @students.empty?
+        puts "No students available".center(@width)
+    else
+        print_header
+        print_students_list
+        print_footer
+    end
 end
 
 def interactive_menu
@@ -49,17 +60,17 @@ def process(selection)
         when "9"
             exit
         else
-            puts "I don't know what you mean"
+            puts "I don't know what you mean".center(@width)
     end
 end
 
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish press return twice"
+    puts "Please enter the names of the students".center(@width)
+    puts "To finish press return twice".center(@width)
     name = STDIN.gets.chomp
     while !name.empty? do
         add_student(name)
-        puts "Now we have #{@students.count} students"
+        puts "Now we have #{@students.count} students".center(@width)
         name = STDIN.gets.chomp
     end
 end
@@ -84,15 +95,18 @@ end
 
 def try_load_students
     filename = ARGV.first 
-    return if filename.nil? 
-    if File.exist?(filename) 
+    if filename.nil? 
+        puts "Loaded default file: students.csv".center(@width)
+        load_students
+    elsif File.exist?(filename) 
       load_students(filename)
-      puts "Loaded #{@students.count} from #{filename}"
+      puts "Loaded #{@students.count} from #{filename}".center(@width)
     else 
-      puts "Sorry, #{filename} doesn't exist."
+      puts "Sorry, #{filename} doesn't exist.".center(@width)
       exit 
     end
 end
+
 
 def add_student(name)
     @students << {name: name, cohort: :november}
