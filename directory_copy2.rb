0b73766974c2c6f5309858d1ab8396d97af3cbd9
Moeid Haider <moeidhaider9@gmail.com>
2023-01-11 21:47:36 +0000

    added a method to add students to avoid repeating code

---
+++ directory_copy2.rb
@@ -0,0 +1,100 @@
+@students = []
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+end
+
+def print_footer
+    puts "Overall, we have #{@students.count} great students"
+end
+
+def print_students_list
+    @students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
+    puts "9. Exit"
+end
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
+end
+
+def interactive_menu
+    loop do
+        print_menu
+        process(STDIN.gets.chomp)
+    end
+end
+
+def process(selection)
+    case selection
+        when "1"
+            input_students
+        when "2"
+            show_students
+        when "3"
+            save_students
+        when "4"
+            load_students
+        when "9"
+            exit
+        else
+            puts "I don't know what you mean"
+    end
+end
+
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish press return twice"
+    name = STDIN.gets.chomp
+    while !name.empty? do
+        add_student(name)
+        puts "Now we have #{@students.count} students"
+        name = STDIN.gets.chomp
+    end
+end
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
+
+def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
+    file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+        add_student(name)
+    end
+    file.close
+end
+
+def try_load_students
+    filename = ARGV.first 
+    return if filename.nil? 
+    if File.exist?(filename) 
+      load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+    else 
+      puts "Sorry, #{filename} doesn't exist."
+      exit 
+    end
+end
+
+def add_student(name)
+    @students << {name: name, cohort: :november}
+end
+interactive_menu
